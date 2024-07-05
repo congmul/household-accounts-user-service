29f7467696012abfede07f296a442cbb143b23b3
Jehyun Jung <congmul@hotmail.com>
2024-07-04 17:13:36 -0700

    Add validator for env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,18 +1,18 @@
-import { cleanEnv, makeValidator } from 'envalid'
-import { config } from 'dotenv'
+import { cleanEnv, makeValidator } from "envalid";
+import { config } from "dotenv";
 
 config();
 
 const noEmptyString = makeValidator((str) => {
-    if(!str) {
-        throw new Error("Cannot be an empty string");
-    } else {
-        str;
-    }
-})
+  if (!str) {
+    throw new Error("Cannot be an empty string");
+  } else {
+    str
+  }
+});
 
 const env = cleanEnv(process.env, {
-    CLIENT_ID: noEmptyString()
-})
+  CLIENT_ID: noEmptyString(),
+});
 
 export default env;
