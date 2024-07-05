e03d02cf7f5e608807284d40788bbea3c3b4646e
Jehyun Jung <congmul@hotmail.com>
2024-07-05 14:00:41 -0700

    Use logger instead of console

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,11 @@
 import { connect } from "mongoose";
 import config from "./config";
+import logger from "../utils/logger";
 
 function dbLoader() {
   connect(config.db.URI, { dbName: config.db.DB_NAME })
-    .then(() => console.log("Application connected to DB"))
-    .catch((err) => console.log("MongoDB connection issue", err));
+    .then(() => logger.info("Application connected to DB"))
+    .catch((err) => logger.error("MongoDB connection issue", err));
 }
 
 export default dbLoader;
