10de89f8637a1b528bd4c6d52e1f83f164c64f49
Jehyun Jung <congmul@hotmail.com>
2024-07-04 21:54:48 -0700

    Add health route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import config from "./config/config";
 
 const app = express();
 
@@ -7,9 +8,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, world!");
+app.get("/health", (req: Request, res: Response) => {
+  res.send({
+    msg: "Account Service is healthy and ready to handle your requests.",
+  });
 });
 
 // Start the server on the port
-app.listen(3000, () => console.log(`Listening on PORT: 3000`));
+app.listen(3000, () => console.log(`Listening on PORT: ${config.PORT}`));
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import { cleanEnv, makeValidator } from "envalid";
+import { cleanEnv, str, makeValidator } from "envalid";
 import { config } from "dotenv";
 
 config();
@@ -7,12 +7,13 @@ const noEmptyString = makeValidator((str) => {
   if (!str) {
     throw new Error("Cannot be an empty string");
   } else {
-    str;
+    return str;
   }
 });
 
 const env = cleanEnv(process.env, {
   CLIENT_ID: noEmptyString(),
+  PORT: str({ default: "3000" }),
 });
 
 export default env;
